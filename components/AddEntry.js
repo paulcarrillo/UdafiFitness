f8a86efa7e3cf1c0e9236bff6f647d7fe803b93f
Paul Carrillo <paulcarrillo05@gmail.com>
2017-11-25 17:38:55 -0700

    Created the slidder option for the application. Buttons added that can plus or minus miles or meters.

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,20 @@
   import React, { Component } from 'react'
-  import { View, Text } from 'react-native'
-  import { getMetricMetaInfo } from '../utils/helpers'
+  import { View, TouchableOpacity, Text } from 'react-native'
+  import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+  import UdaciSlider from './UdaciSlider'
+  import UdaciSteppers from './UdaciSteppers'
+  import DateHeader from './DateHeader'
+  import { Ionicons } from '@expo/vector-icons'
+  import TextButton from './TextButton'
+
+  function SubmitBtn ({ onPress }) {
+    return (
+      <TouchableOpacity
+        onPress={onPress}>
+          <Text>SUBMIT</Text>
+      </TouchableOpacity>
+    )
+  }
 
   export default class AddEntry extends Component {
     state = {
@@ -38,12 +52,81 @@
         [metric]: value,
       }))
     }
+    submit = () => {
+      const key = timeToString()
+      const entry = this.state
 
+      // Update Redux
+
+      this.setState(() => ({
+        run: 0,
+        bike: 0,
+        swim: 0,
+        sleep: 0,
+        eat: 0,
+      }))
+
+      // Navigate to home
+
+      // Save to DB
+
+      // Clear Local notification
+    }
+
+    reset = () => {
+      const key = timeToString()
+
+      // Update Redux
+
+      // Route to Home
+
+      // Update 'DB'
+    }
 
     render() {
+      const metaInfo = getMetricMetaInfo()
+
+      if (this.props.alreadyLogged) {
+        return (
+          <View>
+            <Ionicons
+              name='ios-happy-outline'
+              size={100}
+            />
+            <Text>You alredy logged your information for today</Text>
+            <TextButton onPress={this.reset}>
+              Reset
+            </TextButton>
+          </View>
+        )
+      }
+
       return (
         <View>
-          {getMetricMetaInfo('bike').getIcon('bike')}
+          <DateHeader date={(new Date()).toLocaleDateString()}/>
+          {Object.keys(metaInfo).map((key) => {
+            const { getIcon, type, ...rest } = metaInfo[key]
+            const value = this.state[key]
+
+            return (
+              <View key={key}>
+                {getIcon()}
+                {type === 'slider'
+                ? <UdaciSlider
+                    value={value}
+                    onChange={(value) => this.slide(key, value)}
+                    {...rest}
+                  />
+                : <UdaciSteppers
+                  value={value}
+                  onIncrement={() => this.increment(key)}
+                  onDecrement={() => this.decrement(key)}
+                  {...rest}
+                />}
+              </View>
+            )
+          })}
+          <SubmitBtn onPress={this.submit} />
         </View>
       )
     }
